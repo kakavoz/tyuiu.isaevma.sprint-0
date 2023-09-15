cc0ae288d2ade03bdd6c1003a21f2e088d7f5ad0
maxim isaev <Microsoft10max@gmail.com>
2023-09-15 23:43:17 +0500

    task1

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task1
+namespace task2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "kupri";
-            int age = 20;
-            Console.WriteLine("privet, menya zovut " + name + " mne " + age + " let!");
-            Console.ReadKey();
-
         }
     }
 }
---
+++ task2test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("task2test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task2test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b895b92e-7abc-48f2-8ac3-a56f48bae3d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
